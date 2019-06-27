a5bc44116ed51ef67684b6890c461b877f8dc12c
BetaVN <anhquan971998@gmail.com>
2019-06-27 11:56:47 +0700

    Implement basic calls for text editor

--- app/src/main/java/com/example/memokeeper/MemoEditActivity.java
+++ app/src/main/java/com/example/memokeeper/MemoEditActivity.java
@@ -1,6 +1,33 @@
 package com.example.memokeeper;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MemoEditActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar editToolbar = findViewById(R.id.memoEditBar);
+        EditText textField = findViewById(R.id.memoEditText);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.action_save:
+                //Todo: Add save function
+                return true;
+
+            case R.id.action_exit:
+                //Todo: Add exit function
+                return true;
+
+            default:
+                return false;
+        }
+    }
 }
