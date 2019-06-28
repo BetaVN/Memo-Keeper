d6d9ad67681947922ded2b50b80ddaced09633b8
BetaVN <anhquan971998@gmail.com>
2019-06-28 11:38:00 +0700

    Add title text field

---
+++ app/src/main/java/com/example/memokeeper/Constants/INTENT_CODE.java
@@ -0,0 +1,5 @@
+package com.example.memokeeper.Constants;
+
+public class INTENT_CODE {
+    public static final int MEMO_EDIT = 10000;
+}
--- app/src/main/java/com/example/memokeeper/MemoEditActivity.java
+++ app/src/main/java/com/example/memokeeper/MemoEditActivity.java
@@ -3,16 +3,30 @@ package com.example.memokeeper;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.EditText;
 
 public class MemoEditActivity extends AppCompatActivity {
+    private MenuInflater inflater;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_edit_memo);
         Toolbar editToolbar = findViewById(R.id.memoEditBar);
         EditText textField = findViewById(R.id.memoEditText);
+        setSupportActionBar(editToolbar);
+        getSupportActionBar().setTitle("Memo Keeper");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_memo_edit, menu);
+        return true;
     }
 
     @Override
@@ -20,14 +34,20 @@ public class MemoEditActivity extends AppCompatActivity {
         switch (item.getItemId()){
             case R.id.action_save:
                 //Todo: Add save function
+                sampleEnd();
                 return true;
 
             case R.id.action_exit:
                 //Todo: Add exit function
+                sampleEnd();
                 return true;
 
             default:
                 return false;
         }
+    }
+
+    private void sampleEnd(){
+        super.finish();
     }
 }
