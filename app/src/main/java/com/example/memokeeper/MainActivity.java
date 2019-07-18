b1836419132d0d1d62e5d8d49764e3e9466759a4
BetaVN <anhquan971998@gmail.com>
2019-07-18 11:52:44 +0700

    Display memo on the main screen

--- app/src/main/java/com/example/memokeeper/MainActivity.java
+++ app/src/main/java/com/example/memokeeper/MainActivity.java
@@ -4,30 +4,51 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.memokeeper.MainScreen.MemoAdapter;
+import com.example.memokeeper.MainScreen.MemoInfo;
 import com.example.memokeeper.MemoEditor.MemoEditActivity;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    final private Context CONTEXT = this;
+    final private Context context = this;
+
+    private MemoAdapter memoAdapter;
+    private ArrayList<MemoInfo> memo;
+    private RecyclerView memoList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button testButton = findViewById(R.id.button);
+        //Button testButton = findViewById(R.id.button);
 
 
-        testButton.setOnClickListener(new View.OnClickListener() {
+        /*testButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent modeSelection = new Intent(CONTEXT, MemoEditActivity.class);
                 startActivity(modeSelection);
             }
-        });
+        });*/
+
+        memoList = findViewById(R.id.memoList);
+        memo = new ArrayList<>();
+
+        memo.add(new MemoInfo("This is a sample memo", "18/7/2019"));
+        memo.add(new MemoInfo("This is another sample memo", "19/7/2018"));
+
+
+        memoAdapter = new MemoAdapter(memo, context);
+        memoList.setAdapter(memoAdapter);
+        memoList.setLayoutManager(new LinearLayoutManager(this));
     }
 
 }
--- app/src/main/java/com/example/memokeeper/MainScreen/MemoAdapter.java
+++ app/src/main/java/com/example/memokeeper/MainScreen/MemoAdapter.java
@@ -3,17 +3,27 @@ package com.example.memokeeper.MainScreen;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.memokeeper.MemoEditor.AttachItemAdapter;
+import com.example.memokeeper.R;
 
 import java.util.ArrayList;
 
 public class MemoAdapter extends RecyclerView.Adapter<MemoAdapter.MemoHolder> {
 
     public class MemoHolder extends RecyclerView.ViewHolder {
+        public TextView title;
+        public TextView date;
 
         public MemoHolder(@NonNull View memoView) {
             super(memoView);
+
+            title = memoView.findViewById(R.id.titleName);
+            date = memoView.findViewById(R.id.date);
         }
     }
 
@@ -27,18 +37,24 @@ public class MemoAdapter extends RecyclerView.Adapter<MemoAdapter.MemoHolder> {
 
     @NonNull
     @Override
-    public MemoHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+    public MemoHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View memoView = inflater.inflate(R.layout.item_memo, parent, false);
+        MemoAdapter.MemoHolder newMemo = new MemoAdapter.MemoHolder(memoView);
+        return newMemo;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MemoHolder memoHolder, int i) {
-
+    public void onBindViewHolder(@NonNull final MemoHolder memoHolder, final int position) {
+        final MemoInfo newMemo = memoList.get(position);
+        memoHolder.title.setText(newMemo.memoTitle);
+        memoHolder.date.setText(newMemo.date);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return memoList.size();
     }
 
 
